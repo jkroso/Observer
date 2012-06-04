6b068fe73e7b467a618b66ce114a26fca46203c3
Jakeb Rosoman <jkroso@gmail.com>
2012-06-04 23:07:14 +1200

    Now it has structure

--- test/Observer_test.js
+++ test/Observer_test.js
@@ -21,37 +21,32 @@ define(['../lib/Observer'], function(Observer) { 'use strict';
 			notStrictEqual(actual, expected, [message])
 			raises(block, [expected], [message])
 	*/
+	
 	var subject
+	
 	function yup () {
 		ok(true, 'message received')
 	}
 
-	module('Subscribe', {
+	module('', {
 		setup : function () {
-			subject = new Observer.constructor()
+			subject = new Observer()
 		},
 		teardown : function () {
 			subject = null
 		}
 	})
+
 	test('Can subscribe', function () {
-		expect(1)
-		subject.subscribe('test', yup)
-		subject.publish('test')
+		expect(0)
+		try {
+			subject.subscribe('test', yup)
+		} catch (e) {
+			ok(false)
+		}
+
 	})
-	test('Can subscribe anonamous function', function () {
-		expect(1)
-		subject.subscribe('test', function () {
-			ok(true)
-		})
-		subject.publish('test')
-	})
-	test('can multisubscribe at one time', function () {
-		expect(2)
-		subject.subscribe('a b', yup)
-		subject.publish('a')
-		subject.publish('b')
-	})
+
 	test( "multiple subscriptions", function() {
 		expect( 4 );
 
@@ -74,56 +69,6 @@ define(['../lib/Observer'], function(Observer) { 'use strict';
 		subject.publish( "sub-b-3" );
 	})
 
-
-
-
-
-
-	module('Publish', {
-		setup : function () {
-			subject = new Observer.constructor()
-		},
-		teardown : function () {
-			subject = null
-		}
-	})
-	test('Can prevent lower priority callbacks', function () {
-		expect(0)
-		subject.subscribe('test', yup, 9)
-		subject.subscribe('test', function () {
-			return false
-		}, 1)
-		subject.publish('test')
-	})
-	test('Wont prevent higher priority callbacks', function () {
-		expect(1)
-		subject.subscribe('test', yup, 9)
-		subject.subscribe('test', function () {
-			return false
-		}, 10)
-		subject.publish('test')
-	})
-	test('Returns a custom value when intercepting', function () {
-		expect(1)
-		subject.subscribe('test', function () {
-			return 'suprise'
-		}, 10)
-		equal(subject.publish('test'), 'suprise', 'Return the same value as the intercepting callback')
-	})
-
-
-
-
-
-
-	module('Unsubscribe', {
-		setup : function () {
-			subject = new Observer.constructor()
-		},
-		teardown : function () {
-			subject = null
-		}
-	})
 	test( "various ways of unsubscribing a specific function", function() {
 		expect( 4 );
 		var order = 0;
@@ -161,6 +106,7 @@ define(['../lib/Observer'], function(Observer) { 'use strict';
 		}
 		subject.publish( "unsubscribe" );
 	})
+
 	test('Can unsubscribe anonamous functions while leaving named functions', function () {
 		expect(2)
 		subject.subscribe('test', function () {
@@ -173,6 +119,7 @@ define(['../lib/Observer'], function(Observer) { 'use strict';
 		subject.unsubscribe('test', yup)
 		subject.publish('test')
 	})
+
 	test('can multi-unsubscribe', function () {
 		expect(1)
 		subject.subscribe('a b c', yup)
@@ -181,140 +128,120 @@ define(['../lib/Observer'], function(Observer) { 'use strict';
 		subject.publish('b')
 		subject.publish('a')
 	})
-	test('Can clear all callbacks in one go', function () {
-		expect(2)
-		subject.subscribe('test', function () {
-			ok(true, 'anonamous function ran')
-		})
-		subject.subscribe('test', yup)
-		subject.publish('test')
-		subject.unsubscribe('test')
-		subject.publish('test')
-	})
+
 	test( "unsubscribe during publish", function() {
-		expect( 3 );
+		expect( 3 )
 
 		function racer() {
-			ok( true, "second" );
-			subject.unsubscribe( "racy", racer );
+			ok( true, "second" )
+			subject.unsubscribe( "racy", racer )
 		}
 
 		subject.subscribe( "racy", function() {
-			ok( true, "first" );
+			ok( true, "first" )
 		});
-		subject.subscribe( "racy", racer );
+		subject.subscribe( "racy", racer )
 		subject.subscribe( "racy", function() {
-			ok( true, "third" );
+			ok( true, "third" )
 		});
-		subject.publish( "racy" );
+		subject.publish( "racy" )
 	})
 
-
-
-	module('Others', {
-		setup : function () {
-			subject = new Observer.constructor()
-		},
-		teardown : function () {
-			subject = null
-		}
-	})
 	test( "continuation", function() {
-		expect( 7 );
+		expect( 7 )
 		subject.subscribe( "continuation", function() {
-			ok( true, "first subscriber called" );
+			ok( true, "first subscriber called" )
 		});
 		subject.subscribe( "continuation", function() {
-			ok( true, "continued after no return value" );
+			ok( true, "continued after no return value" )
 			return true;
-		});
+		})
 		strictEqual( subject.publish( "continuation" ), true,
-			"return true when subscriptions are not stopped" );
+			"return true when subscriptions are not stopped" )
 
-		subject.subscribe( "continuation", function() {
-			ok( true, "continued after returning true" );
-			return false;
+		subject.subscribe( "continuation", function(event) {
+			ok( true, "continued after returning true" )
+			event.stopPropagation()
 		});
 		subject.subscribe( "continuation", function() {
-			ok( false, "continued after returning false" );
-		});
+			ok( false, "continued after stopPropagation" )
+		})
 		strictEqual( subject.publish( "continuation" ), false,
-			"return false when subscriptions are stopped" );
-	});
+			"return false when subscriptions are stopped" )
+	})
 
 	test( "priority", function() {
-		expect( 5 );
-		var order = 0;
-
+		expect( 5 )
+		var order = 0
 		subject.subscribe( "priority", function() {
-			strictEqual( order, 1, "priority default; #1" );
-			order++;
-		});
+			strictEqual( order, 1, "priority default; #1" )
+			order++
+		})
 		subject.subscribe( "priority", function() {
-			strictEqual( order, 3, "priority 15; #1" );
-			order++;
-		}, 15 );
+			strictEqual( order, 3, "priority 15; #1" )
+			order++
+		}, {priority:15} )
 		subject.subscribe( "priority", function() {
-			strictEqual( order, 2, "priority default; #2" );
-			order++;
-		});
+			strictEqual( order, 2, "priority default; #2" )
+			order++
+		})
 		subject.subscribe( "priority", function() {
-			strictEqual( order, 0, "priority 1; #1" );
-			order++;
-		}, 1 );
-		subject.subscribe( "priority", {}, function() {
-			strictEqual( order, 4, "priority 15; #2" );
-			order++;
-		}, 15 );
-		subject.publish( "priority" );
-	});
+			strictEqual( order, 0, "priority 1; #1" )
+			order++
+		}, {priority:1} )
+		subject.subscribe( "priority", function() {
+			strictEqual( order, 4, "priority 15; #2" )
+			order++
+		}, {priority:15} )
+		subject.publish( "priority" )
+	})
 
 	test( "context", function() {
-		expect( 3 );
+		expect( 3 )
 		var obj = {},
-			fn = function() {};
+			fn = function() {}
 
 		subject.subscribe( "context", function() {
-			strictEqual( this, window, "default context" );
-		});
-		subject.subscribe( "context", obj, function() {
-			strictEqual( this, obj, "object" );
-		});
-		subject.subscribe( "context", fn, function() {
-			strictEqual( this, fn, "function" );
-		});
-		subject.publish( "context" );
+			strictEqual( this, window, "default context" )
+		})
+		subject.subscribe( "context", function() {
+			strictEqual( this, obj, "object" )
+		}, {context:obj})
+		subject.subscribe( "context", function() {
+			strictEqual( this, fn, "function" )
+		}, {context:fn})
+		subject.publish( "context" )
 	});
 
 	test( "data", function() {
-		subject.subscribe( "data", function( string, number, object ) {
-			strictEqual( string, "hello", "string passed" );
-			strictEqual( number, 5, "number passed" );
-			deepEqual( object, {
+		subject.subscribe( "data", function( event ) {
+			strictEqual( event.data.string, "hello", "string passed" )
+			strictEqual( event.data.number, 5, "number passed" )
+			deepEqual( event.data.object, {
 				foo: "bar",
 				baz: "qux"
-			}, "object passed" );
-			string = "goodbye";
-			object.baz = "quux";
+			}, "object passed" )
+			event.data.string = "goodbye"
+			event.data.object.baz = "quux"
 		});
-		subject.subscribe( "data", function( string, number, object ) {
-			strictEqual( string, "hello", "string unchanged" );
-			strictEqual( number, 5, "number unchanged" );
-			deepEqual( object, {
+		subject.subscribe( "data", function( event ) {
+			strictEqual( event.data.string, "goodbye", "string changed" )
+			strictEqual( event.data.number, 5, "number unchanged" )
+			deepEqual( event.data.object, {
 				foo: "bar",
 				baz: "quux"
-			}, "object changed" );
+			}, "object changed" )
 		});
 
 		var obj = {
 			foo: "bar",
 			baz: "qux"
 		};
-		subject.publish( "data", "hello", 5, obj );
+		subject.publish( "data", {string: "hello", number:5, object: obj} )
 		deepEqual( obj, {
 			foo: "bar",
 			baz: "quux"
-		}, "object updated" );
-	});
+		}, "object updated" )
+	})
 
-});
+})
