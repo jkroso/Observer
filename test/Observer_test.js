447fefac8d2dd3ea2bfb4ed01ed9f3a9a099e13a
Jakeb Rosoman <jkroso@gmail.com>
2012-07-16 02:45:01 +1200

    Added mixin test

--- test/Observer_test.js
+++ test/Observer_test.js
@@ -40,49 +40,64 @@ define(['../lib/Observer'], function(Observer) { 'use strict';
 	test('Can subscribe', function () {
 		expect(0)
 		try {
-			subject.subscribe('test', yup)
+			subject.on('test', yup)
 		} catch (e) {
 			ok(false)
 		}
 	})
 
+
 	test( "multiple subscriptions", function() {
 		expect( 4 )
 
-		subject.subscribe( "sub-a-1 sub-a-2 sub-a-3", function() {
+		subject.on( "sub-a-1 sub-a-2 sub-a-3", function() {
 			ok( true )
 		})
 		subject.publish( "sub-a-1" )
 
-		subject.subscribe( "sub-b-1 sub-b-2", function() {
+		subject.on( "sub-b-1 sub-b-2", function() {
 			ok( true )
 		})
 		
 		// Test for Ticket #18
-		subject.subscribe( "sub-b-1 sub-b-3", function() {
+		subject.on( "sub-b-1 sub-b-3", function() {
 			ok( true )
 		})
 		
-		subject.run( "sub-b-2" )
+		subject.publish( "sub-b-2" )
 		subject.publish( "sub-b-2" )
 		subject.publish( "sub-b-3" )
+	})
+
+	test('Mixin', function () {
+		expect(1)
+		var mixee = {}
+		Observer(mixee)
+		mixee.on("sub-a", function() {
+			ok( true )
+		})
+		mixee.publish( "sub-a" )
+		mixee = null
 	})
 
 	test( 'Quick publish A.K.A `run`', function () {
 		expect( 1 )
 
-		subject.subscribe( "sub-a-1", function() {
+		subject.on( "sub-a-1", function() {
+			ok( false )
+		})
+		subject.on( "sub-a-1.lvl2", function() {
 			ok( true )
 		})
 
-		subject.run( "sub-a-1" )
+		subject.run( "sub-a-1.lvl2" )
 	})
 
 	test( "various ways of unsubscribing a specific function", function() {
 		expect( 4 )
 		var order = 0
 
-		subject.subscribe( "unsubscribe", function() {
+		subject.on( "unsubscribe", function() {
 			strictEqual( order, 0, "first subscriber called" )
 			order++
 		})
@@ -90,25 +105,25 @@ define(['../lib/Observer'], function(Observer) { 'use strict';
 			ok( false, "removed by original reference" )
 			order++
 		}
-		subject.subscribe( "unsubscribe", fn )
-		subject.subscribe( "unsubscribe", function() {
+		subject.on( "unsubscribe", fn )
+		subject.on( "unsubscribe", function() {
 			strictEqual( order, 1, "second subscriber called" )
 			order++
 		})
-		var fn2 = subject.subscribe( "unsubscribe", function() {
+		var fn2 = subject.on( "unsubscribe", function() {
 			ok( false, "removed by returned reference" )
 			order++
 		})
-		subject.unsubscribe( "unsubscribe", fn )
-		subject.unsubscribe( "unsubscribe", fn2 )
+		subject.off( "unsubscribe", fn )
+		subject.off( "unsubscribe", fn2 )
 		try {
-			subject.unsubscribe( "unsubscribe", function() {})
+			subject.off( "unsubscribe", function() {})
 			ok( true, "no error with invalid handler" )
 		} catch ( e ) {
 			ok( false, "error with invalid handler" )
 		}
 		try {
-			subject.unsubscribe( "unsubscribe2", function() {})
+			subject.off( "unsubscribe2", function() {})
 			ok( true, "no error with invalid topic" )
 		} catch ( e ) {
 			ok( false, "error with invalid topic" )
@@ -118,21 +133,21 @@ define(['../lib/Observer'], function(Observer) { 'use strict';
 
 	test('Can unsubscribe anonamous functions while leaving named functions', function () {
 		expect(2)
-		subject.subscribe('test', function () {
+		subject.on('test', function () {
 			ok(true, 'anonamous function ran')
 		})
 		subject.publish('test')
-		subject.subscribe('test', yup)
-		subject.unsubscribe('test', '')
+		subject.on('test', yup)
+		subject.off('test', '')
 		subject.publish('test')
-		subject.unsubscribe('test', yup)
+		subject.off('test', yup)
 		subject.publish('test')
 	})
 
 	test('can multi-unsubscribe', function () {
 		expect(1)
-		subject.subscribe('a b c', yup)
-		subject.unsubscribe('a b', yup)
+		subject.on('a b c', yup)
+		subject.off('a b', yup)
 		subject.publish('c')
 		subject.publish('b')
 		subject.publish('a')
@@ -146,36 +161,36 @@ define(['../lib/Observer'], function(Observer) { 'use strict';
 			strictEqual(order, 2, 'subs not affected')
 		}
 
-		subject.subscribe( "racy", function() {
+		subject.on( "racy", function() {
 			strictEqual(order, 0)
 			order++
 		})
-		subject.subscribe( "racy", function () {
-			subject.unsubscribe( "racy", racer )
+		subject.on( "racy", function () {
+			subject.off( "racy", racer )
 			strictEqual(order, 1)
 			order++
 		})
-		subject.subscribe( "racy", racer)
+		subject.on( "racy", racer)
 		subject.publish( "racy" )
 	})
 
 	test( "continuation", function() {
 		expect( 7 )
-		subject.subscribe( "continuation", function() {
+		subject.on( "continuation", function() {
 			ok( true, "first subscriber called" )
 		})
-		subject.subscribe( "continuation", function() {
+		subject.on( "continuation", function() {
 			ok( true, "continued after no return value" )
 			return true
 		})
 		strictEqual( subject.publish( "continuation" ), true,
 			"return true when subscriptions are not stopped" )
 
-		subject.subscribe( "continuation", function(event) {
+		subject.on( "continuation", function(event) {
 			ok( true, "continued after returning true" )
 			return false
 		})
-		subject.subscribe( "continuation", function() {
+		subject.on( "continuation", function() {
 			ok( false, "continued after returning false" )
 		})
 		strictEqual( subject.publish( "continuation" ), false,
@@ -185,27 +200,27 @@ define(['../lib/Observer'], function(Observer) { 'use strict';
 	test( "priority", function() {
 		expect( 6 )
 		var order = 0
-		subject.subscribe( "priority", function() {
+		subject.on( "priority", function() {
 			strictEqual( order, 3, "priority default; #1" )
 			order++
 		})
-		subject.subscribe( "priority", function() {
+		subject.on( "priority", function() {
 			strictEqual( order, 1, "priority 1; #1" )
 			order++
 		}, 1 )
-		subject.subscribe( "priority", function() {
+		subject.on( "priority", function() {
 			strictEqual( order, 4, "priority default; #2" )
 			order++
 		})
-		subject.subscribe( "priority", function() {
+		subject.on( "priority", function() {
 			strictEqual( order, 0, "priority 10; #1" )
 			order++
 		}, 10 )
-		subject.subscribe( "priority", function() {
+		subject.on( "priority", function() {
 			strictEqual( order, 2, "priority 1; #2" )
 			order++
 		}, 1 )
-		subject.subscribe( "priority", function() {
+		subject.on( "priority", function() {
 			strictEqual( order, 5, "priority -1; #1" )
 		}, -Infinity )
 		subject.publish( "priority" )
@@ -216,20 +231,20 @@ define(['../lib/Observer'], function(Observer) { 'use strict';
 		var obj = {},
 			fn = function() {}
 
-		subject.subscribe( "context", function() {
+		subject.on( "context", function() {
 			strictEqual( this, window, "default context" )
 		})
-		subject.subscribe( "context", obj,function() {
+		subject.on( "context", obj,function() {
 			strictEqual( this, obj, "object" )
 		})
-		subject.subscribe( "context", fn, function() {
+		subject.on( "context", fn, function() {
 			strictEqual( this, fn, "function" )
 		})
 		subject.publish( "context" )
 	})
 
 	test( "data", function() {
-		subject.subscribe( "data", function( data ) {
+		subject.on( "data", function( data ) {
 			strictEqual( data.string, "hello", "string passed" )
 			strictEqual( data.number, 5, "number passed" )
 			deepEqual( data.object, {
@@ -239,7 +254,7 @@ define(['../lib/Observer'], function(Observer) { 'use strict';
 			data.string = "goodbye"
 			data.object.baz = "quux"
 		})
-		subject.subscribe( "data", function( data ) {
+		subject.on( "data", function( data ) {
 			strictEqual( data.string, "goodbye", "string changed" )
 			strictEqual( data.number, 5, "number unchanged" )
 			deepEqual( data.object, {
@@ -262,23 +277,23 @@ define(['../lib/Observer'], function(Observer) { 'use strict';
 	test('Specificity ordering', function() {
 		expect(5)
 		var order = 0
-		subject.subscribe('a.b.c.d', function (data) {
+		subject.on('a.b.c.d', function (data) {
 			strictEqual(order, 0, 'fourth level')
 			order++
 		})
-		subject.subscribe('a.b.c', function (data) {
+		subject.on('a.b.c', function (data) {
 			strictEqual(order, 1, 'third level')
 			order++
 		})
-		subject.subscribe('a.b', function (data) {
+		subject.on('a.b', function (data) {
 			strictEqual(order, 2, 'second level')
 			order++
 		})
-		subject.subscribe('a', function (data) {
+		subject.on('a', function (data) {
 			strictEqual(order, 3, 'first level')
 			order++
 		})
-		subject.subscribe(function (data) {
+		subject.on(function (data) {
 			strictEqual(order, 4, 'top level no topic')
 		})
 		subject.publish('a.b.c.d.e.f.g', 'Some data')
